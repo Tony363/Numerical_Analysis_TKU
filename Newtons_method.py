5f1f81bae3e7b77418d409b024a5471806eaf13e
Tony Siu <thefool363@gmail.com>
2021-11-02 05:33:16 +0000

    added newtons method

---
+++ Newtons_method.py
@@ -0,0 +1,44 @@
+# Defining Function
+def f(x):
+    return x**3 - 5*x - 9
+
+# Defining derivative of function
+def g(x):
+    return 3*x**2 - 5
+
+# Implementing Newton Raphson Method
+
+def newtonRaphson(x0,e,N):
+    print('\n\n*** NEWTON RAPHSON METHOD IMPLEMENTATION ***')
+    step = 1
+    flag = 1
+    condition = True
+    while condition:
+        if g(x0) == 0.0:
+            print('Divide by zero error!')
+            break
+        
+        x1 = x0 - f(x0)/g(x0)
+        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, x1, f(x1)))
+        x0 = x1
+        step = step + 1
+        
+        if step > N:
+            flag = 0
+            break
+        
+        condition = abs(f(x1)) > e
+    
+    if flag==1:
+        print('\nRequired root is: %0.8f' % x1)
+    else:
+        print('\nNot Convergent.')
+
+if __name__ == "__main__":
+    #Note: You can combine above three section like this
+    x0 = float(input('Enter Guess: '))
+    e = float(input('Tolerable Error: '))
+    N = int(input('Maximum Step: '))
+
+    # Starting Newton Raphson Method
+    newtonRaphson(x0,e,N)
